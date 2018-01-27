111dc95c2ceeb55664a3e3b2009e65675b3482d1
Gena <gena@e64.net>
2018-01-27 16:59:19 +0100

    envoi de mail avec jQuery

--- contact.php
+++ contact.php
@@ -9,11 +9,19 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $mailDestinataire ="gena@e64.net";
-
+$subject = "Vous avez reçu un nouveau message de $name";
+$subject ="=?UTF-8?B?".base64_encode($subject)."?=";
+$entetes =
+    'Content-type: text/html; charset=utf-8' . "\r\n" .
+    "From: $email" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 //mail($name, $email, $message, $mailDestinataire);
-mail ($mailDestinataire, $name, $message);
+if(mail ($mailDestinataire, $subject, $message, $entetes)){
+    die("Votre message a bien été envoyé. Merci");
+}else{
+    die("Votre message n'a pas être envoyé, merci de réessayer ultérireurement.");
+}
 //mail ($to, $subject, $message, $additional_headers = null, $additional_parameters = null)
 ?>
-
-
 
